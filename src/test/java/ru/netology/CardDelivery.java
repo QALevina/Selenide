b89d3a7730b08d814315405366f58e53d5c4b5df
Elena Levina <qa-levina@yandex.ru>
2022-09-18 11:25:09 +0300

    Corrected task

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -22,12 +22,16 @@ public class CardDelivery {
         open("http://localhost:9999");
     }
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
 
     @Test
     void happyPath() {
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
         $x("//*[@name='name']").setValue("Елена Левина-Иванова");
         $x("//*[@name='phone']").setValue("+79998887766");
         $x("//*[@data-test-id='agreement']").click();
@@ -38,11 +42,21 @@ public class CardDelivery {
         String text = $x("//*[@class='notification__title']").getText();
         assertEquals("Успешно!", text);
 
+        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        String planningDateText = $x("//div[@class='notification__content']").getText();
+        assertEquals("Встреча успешно забронирована на " + planningDate, planningDateText);
+
     }
+    
 
     @Test
     void invalidCity() {
-        $x("//*[@placeholder='Город']").setValue("Мос");
+        String planningDate = generateDate(3);
+        $x("//*[@placeholder='Город']").doubleClick().sendKeys(Keys.DELETE + "Moc");
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
+        $x("//*[@name='name']").setValue("Елена Левина-Иванова");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//*[@data-test-id='city']").getText();
         assertEquals("Доставка в выбранный город недоступна", text);
@@ -51,7 +65,12 @@ public class CardDelivery {
 
     @Test
     void invalidNoCity() {
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("");
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
+        $x("//*[@name='name']").setValue("Елена Левина-Иванова");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//*[@data-test-id='city']").getText();
         assertEquals("Поле обязательно для заполнения", text);
@@ -60,9 +79,12 @@ public class CardDelivery {
 
     @Test
     void selectPastDate() {
+        String planningDate = generateDate(2);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().minusDays(30).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
+        $x("//*[@name='name']").setValue("Елена Левина-Иванова");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//span[contains(@data-test-id,'date')]//span[@class='input__sub']").getText();
         assertEquals("Заказ на выбранную дату невозможен", text);
@@ -70,9 +92,29 @@ public class CardDelivery {
     }
 
     @Test
+    void selectFutureDate() {
+        String planningDate = generateDate(4);
+        $x("//*[@placeholder='Город']").setValue("Москва");
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
+        $x("//*[@name='name']").setValue("Елена Левина-Иванова");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[@class='button__text']").click();
+        $x("//form").should(Condition.visible, Duration.ofSeconds(15));
+
+        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        String planningDateText = $x("//div[@class='notification__content']").getText();
+        assertEquals("Встреча успешно забронирована на " + planningDate, planningDateText);
+
+    }
+
+    @Test
     void invalidNoDate() {
         $x("//*[@placeholder='Город']").setValue("Москва");
         $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE);
+        $x("//*[@name='name']").setValue("Елена Левина-Иванова");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//span[contains(@data-test-id,'date')]//span[@class='input__sub']").getText();
         assertEquals("Неверно введена дата", text);
@@ -81,10 +123,12 @@ public class CardDelivery {
 
     @Test
     void invalidName() {
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
         $x("//*[@name='name']").setValue("Elena");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//span[contains(@data-test-id,'name')]/span/span[contains(@class,'input__sub')]").getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
@@ -92,10 +136,12 @@ public class CardDelivery {
 
     @Test
     void invalidNoName() {
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
         $x("//*[@name='name']").setValue("");
+        $x("//*[@name='phone']").setValue("+79998887766");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//span[contains(@data-test-id,'name')]/span/span[contains(@class,'input__sub')]").getText();
         assertEquals("Поле обязательно для заполнения", text);
@@ -103,11 +149,12 @@ public class CardDelivery {
 
     @Test
     void invalidPhone() {
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
         $x("//*[@name='name']").setValue("Елена Левина-Иванова");
         $x("//*[@name='phone']").setValue("+7999");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//span[contains(@class,'input_invalid')]/span/span[contains(@class,'input__sub')]").getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
@@ -115,11 +162,12 @@ public class CardDelivery {
 
     @Test
     void invalidNoPhone() {
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
         $x("//*[@name='name']").setValue("Елена Левина-Иванова");
         $x("//*[@name='phone']").setValue("");
+        $x("//*[@data-test-id='agreement']").click();
         $x("//*[@class='button__text']").click();
         String text = $x("//span[contains(@data-test-id,'phone')]/span/span[@class='input__sub']").getText();
         assertEquals("Поле обязательно для заполнения", text);
@@ -127,13 +175,11 @@ public class CardDelivery {
 
     @Test
     void invalidNoClikCheckBox() {
-
+        String planningDate = generateDate(3);
         $x("//*[@placeholder='Город']").setValue("Москва");
-        String minData = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + minData);
+        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE + planningDate);
         $x("//*[@name='name']").setValue("Елена Левина-Иванова");
         $x("//*[@name='phone']").setValue("+79998887766");
-
         $x("//*[@class='button__text']").click();
 
         String text = $x("//label[contains(@class,'input_invalid')]/span[contains(@role,'presentation')]").getText();
